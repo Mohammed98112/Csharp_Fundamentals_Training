5658937ef721726a5cca4326dc10f0604945dcf2
Mohammed98112 <seaomn27@gmail.com>
2026-05-12 10:32:13 +0400

    new project

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,18 +9,34 @@ class Program
         Console.Clear();
 
         double angle = 0;
+        ConsoleColor[] colors = {
+            ConsoleColor.Cyan,
+            ConsoleColor.Yellow,
+            ConsoleColor.Magenta,
+            ConsoleColor.Green,
+            ConsoleColor.Red,
+            ConsoleColor.Blue
+        };
+
+        int colorIndex = 0;
 
         while (!Console.KeyAvailable)
         {
             Console.Clear();
 
+            Console.ForegroundColor = colors[colorIndex];
+
             DrawCube(angle);
 
             angle += 0.05;
 
-            Thread.Sleep(40);
+            // change color every frame
+            colorIndex = (colorIndex + 1) % colors.Length;
+
+            Thread.Sleep(60);
         }
 
+        Console.ResetColor();
         Console.CursorVisible = true;
     }
 
@@ -29,26 +45,22 @@ class Program
         int width = Console.WindowWidth;
         int height = Console.WindowHeight;
 
-        double size = 10;
-
         for (int i = -10; i <= 10; i += 2)
         {
             for (int j = -10; j <= 10; j += 2)
             {
-                // 3D point on cube surface
                 double x = i;
                 double y = j;
                 double z = 10;
 
-                // rotate Y axis
+                // rotation Y
                 double x1 = x * Math.Cos(angle) - z * Math.Sin(angle);
                 double z1 = x * Math.Sin(angle) + z * Math.Cos(angle);
 
-                // rotate X axis
+                // rotation X
                 double y1 = y * Math.Cos(angle) - z1 * Math.Sin(angle);
-                double z2 = y * Math.Sin(angle) + z1 * Math.Cos(angle);
 
-                z2 += 30;
+                z1 += 30;
 
                 int screenX = (int)(width / 2 + x1 * 2);
                 int screenY = (int)(height / 2 + y1);
@@ -57,7 +69,6 @@ class Program
                     screenY >= 0 && screenY < height)
                 {
                     Console.SetCursorPosition(screenX, screenY);
-                    Console.ForegroundColor = ConsoleColor.Cyan;
                     Console.Write("*");
                 }
             }
